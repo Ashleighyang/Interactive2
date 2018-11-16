5627e378079626cb683fa9e39a175dd36d710627
Ashleighyang <Ashleigh@github.com>
2018-11-15 16:02:56 -0800

    box project

--- 5 box story/box.js
+++ 5 box story/box.js
@@ -1,21 +1,42 @@
-let box1 = document.querySelector('box1')
+// let box1 = document.querySelector('box1')
 
-box1.addEventListener('click',function() {
+// box1.addEventListener('click',function() {
   
-  this.animate([{
-			background: getComputedStyle(this).background,
-			width: getComputedStyle(this).width,
-			height:  getComputedStyle(this).height
-		},
-    {
-			background: black,
-			width: '1670px',
-			height: '806px'
-		}], {
-      duration: 250,
-      iterations: 1,
-      fill:'forwards'
-      console.log('hello')
-  });
+//   this.animate([{
+// 			background: getComputedStyle(this).background,
+// 			width: getComputedStyle(this).width,
+// 			height:  getComputedStyle(this).height
+// 		},
+//     {
+// 			background: black,
+// 			width: '1670px',
+// 			height: '806px'
+// 		}], {
+//       duration: 250,
+//       iterations: 1,
+//       fill:'forwards'
+//       console.log('hello')
+//   });
 
-})
+// })
+
+$(document).ready(function(){
+    $(".box1").click(function(){
+        $(this).addClass("fullscreen");
+    });
+
+
+ $(".box2").click(function(){
+        $(this).addClass("screen2");
+    });
+
+
+
+ $(".box3").click(function(){
+        $(this).addClass("screen3");
+        $('.box2').addClass("black");
+        $('.box1').addClass("white");
+    });
+
+
+});
